b655e2c77be8e3a626021ced4e49c5ecaa3d0c76
Yehya Awad <yehya@psu.edu>
2015-11-17 23:36:59 -0500

    Initial commit

---
+++ src/meetingschedulingsystem/Person.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015 Yehya Awad
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package meetingschedulingsystem;
+
+/**
+ *
+ * @author Yehya Awad
+ * 
+ */
+
+public class Person {
+    
+    private final String firstName;
+    private final String lastName;
+
+    public Person(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    /**
+     * Get the value of firstName
+     *
+     * @return the value of firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    
+     /**
+     * Get the value of lastName
+     *
+     * @return the value of lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+    
+        private String phoneNumber;
+
+    /**
+     * Get the value of phoneNumber
+     *
+     * @return the value of phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Set the value of phoneNumber
+     *
+     * @param phoneNumber new value of phoneNumber
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
