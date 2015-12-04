1eefbc1c881a80a25e391fe279537ef9ff4eccaa
Yehya Awad <yehya@psu.edu>
2015-12-04 13:15:09 -0500

    Update many changes and additions To name a few: Add listeners for updating table Add functionality for Show and Delete buttons A lot more...

--- src/meetingschedulingsystem/MeetingsMenu.java
+++ src/meetingschedulingsystem/MeetingsMenu.java
@@ -1,22 +1,25 @@
 /*
- * Copyright (C) 2015 yha5009
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* Copyright (C) 2015 yha5009
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package meetingschedulingsystem;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
@@ -31,7 +34,7 @@ public class MeetingsMenu extends javax.swing.JFrame {
     private MeetingTableModel mtm = new MeetingTableModel();
     private ListSelectionModel lsm;
     private int selectedMeetingIndex = -1;
-
+    
     /**
      * Creates new form MeetingsMenu
      */
@@ -40,6 +43,8 @@ public class MeetingsMenu extends javax.swing.JFrame {
         initTable();
         initSettings();
         initSelectionModel();
+        updateModel();
+        initListenerUpdate();
     }
     
     private void initSelectionModel() {
@@ -50,6 +55,13 @@ public class MeetingsMenu extends javax.swing.JFrame {
                 selectedMeetingIndex = lsm.getMinSelectionIndex();
             }
         });
+    }
+    
+    private void showNoMeetingSelected() {
+        JOptionPane.showMessageDialog(this,
+                "Please select a meeting first.",
+                "Meeitng not selected",
+                JOptionPane.INFORMATION_MESSAGE);
     }
     
     private void initSettings() {
@@ -63,13 +75,53 @@ public class MeetingsMenu extends javax.swing.JFrame {
         meetingTable.getTableHeader().setReorderingAllowed(false);
     }
     
-    public void updateModel() {
-        mtm.clearMeetings();
+    /**
+     * Updates table every time it is somehow changed/affected/focused on etc.
+     */
+    private void initListenerUpdate() {
+        this.addWindowStateListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                updateModel();
+            }
+            @Override
+            public void windowStateChanged(WindowEvent e) {
+                updateModel();
+            }
+        });
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                updateModel();
+            }
+            
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                updateModel();
+            }
+            
+            @Override
+            public void windowActivated(WindowEvent e) {
+                updateModel();
+            }
+            
+            @Override
+            public void windowOpened(WindowEvent e) {
+                updateModel();
+            }
+            
+        });
+    }
+    
+    private void updateModel() {
+        mtm.clearData();
         for (Meeting meet : DataManager.getMeetings()) {
             mtm.addMeeting(meet);
         }
+        mtm.refreshTable();
         meetingTable.setModel(mtm);
         meetingTable.getTableHeader().resizeAndRepaint();
+        meetingTable.updateUI();
     }
     
     /**
@@ -82,12 +134,11 @@ public class MeetingsMenu extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel2 = new javax.swing.JLabel();
-        jSeparator1 = new javax.swing.JSeparator();
         jScrollPane1 = new javax.swing.JScrollPane();
         meetingTable = new javax.swing.JTable();
-        AddNewButton = new javax.swing.JButton();
-        DelSelButton = new javax.swing.JButton();
-        ShowSelButton = new javax.swing.JButton();
+        addNewButton = new javax.swing.JButton();
+        delSelButton = new javax.swing.JButton();
+        showSelButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -115,80 +166,126 @@ public class MeetingsMenu extends javax.swing.JFrame {
             }
         });
         meetingTable.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_ALL_COLUMNS);
+        meetingTable.setShowVerticalLines(false);
         jScrollPane1.setViewportView(meetingTable);
 
-        AddNewButton.setText("Add New");
-        AddNewButton.addActionListener(new java.awt.event.ActionListener() {
+        addNewButton.setText("Add New");
+        addNewButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AddNewButtonActionPerformed(evt);
+                addNewButtonActionPerformed(evt);
             }
         });
 
-        DelSelButton.setText("Delete Selected");
-        DelSelButton.addActionListener(new java.awt.event.ActionListener() {
+        delSelButton.setText("Delete All Selected");
+        delSelButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                DelSelButtonActionPerformed(evt);
+                delSelButtonActionPerformed(evt);
             }
         });
 
-        ShowSelButton.setText("Show Selected");
+        showSelButton.setText("Show First Selected");
+        showSelButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                showSelButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jSeparator1)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 369, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(AddNewButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(DelSelButton, javax.swing.GroupLayout.DEFAULT_SIZE, 141, Short.MAX_VALUE)
-                            .addComponent(ShowSelButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addComponent(addNewButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(delSelButton, javax.swing.GroupLayout.DEFAULT_SIZE, 141, Short.MAX_VALUE)
+                            .addComponent(showSelButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(4, 4, 4)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(AddNewButton)
+                        .addComponent(addNewButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(ShowSelButton)
+                        .addComponent(showSelButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(DelSelButton))
+                        .addComponent(delSelButton))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void AddNewButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddNewButtonActionPerformed
+    
+    private void addNewButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addNewButtonActionPerformed
         AddMeetingDialog addDialog = new AddMeetingDialog(this, true);
         addDialog.setVisible(true);
-    }//GEN-LAST:event_AddNewButtonActionPerformed
-
-    private void DelSelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DelSelButtonActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_DelSelButtonActionPerformed
-
+        addDialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                updateModel();
+            }
+        });
+    }//GEN-LAST:event_addNewButtonActionPerformed
+    
+    private void delSelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_delSelButtonActionPerformed
+        if (meetingTable.getSelectedRows().length == 0){
+            showNoMeetingSelected();
+            return;
+        }
+        int [] selectedRows = meetingTable.getSelectedRows();
+        int confirmCancel = JOptionPane.showConfirmDialog(this,
+                "Are you sure you want to cancel the selected meeting(s)?"
+                        + "\nAll attendees will be removed from these meeting.",
+                "Canceling Meeting", JOptionPane.YES_NO_OPTION,
+                JOptionPane.WARNING_MESSAGE);
+        if (confirmCancel == JOptionPane.YES_OPTION) {
+            for (int rowindx: selectedRows) {
+                Meeting selectedMeeting = mtm.getMeetingAtIndex(rowindx);
+                DataManager.removeMeeting(selectedMeeting);
+            }
+            updateModel();
+            JOptionPane.showMessageDialog(this, "Meeting(s) canceled succesfully.");
+        }
+    }//GEN-LAST:event_delSelButtonActionPerformed
+    
+    private void showSelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showSelButtonActionPerformed
+        if (meetingTable.getSelectedRow() < 0){
+            showNoMeetingSelected();
+            return;
+        }
+        int selectedRow = meetingTable.getSelectedRow();
+        Meeting selectedMeeting = mtm.getMeetingAtIndex(selectedRow);
+        ShowMeetingDialog showmeetingd = new ShowMeetingDialog(selectedMeeting);
+        showmeetingd.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                super.windowClosed(e); //To change body of generated methods, choose Tools | Templates.
+                updateModel();
+            }
+        });
+        showmeetingd.setVisible(true);
+    }//GEN-LAST:event_showSelButtonActionPerformed
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+//    public static void main(String args[]) {
 //        /* Set the Nimbus look and feel */
 //        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
 //        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
 //         */
 //        try {
 //            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -214,15 +311,14 @@ public class MeetingsMenu extends javax.swing.JFrame {
 //                new MeetingsMenu().setVisible(true);
 //            }
 //        });
-    }
-
+//    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton AddNewButton;
-    private javax.swing.JButton DelSelButton;
-    private javax.swing.JButton ShowSelButton;
+    private javax.swing.JButton addNewButton;
+    private javax.swing.JButton delSelButton;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTable meetingTable;
+    private javax.swing.JButton showSelButton;
     // End of variables declaration//GEN-END:variables
 }
